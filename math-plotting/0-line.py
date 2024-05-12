fd92c08fa779b5b2d4255e91118c32043c73e911
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-12 04:56:55 -0700

    Plotting

--- math-plotting/0-line.py
+++ math-plotting/0-line.py
@@ -7,18 +7,17 @@ y = np.arange(0, 11) ** 3
 # x values ranging from 0 to 10
 x = np.arange(0, 11)
 
-# Plot y as a solid red line
+#  y as a solid red line
 # 'r-' specifies solid red line
 plt.plot(x, y, 'r-')
 
 # x-axis range from 0 to 10
 plt.xlim(0, 10)
 
-# labels and title
+# Labels and title
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Line Plot of Y')
 
-# Display the plot
+# Display of the plot
 plt.show()
-
