9f41e09155782cf3c39b9308d226b3bb942bd6c6
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-19 05:51:19 -0700

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+"""
+    Calculating the shape of a matrix.
+
+    Parameters:
+    matrix (list): A list of lists (matrix) where each sublist is of the same length.
+
+    Returns:
+    list: A list of integers representing the shape of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
