4bf1af4dfba659924c71f1af4de9548e9f75730b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-19 06:25:45 -0700

    Flip Me Over

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""
+Module for transposing a matrix.
+"""
+
 
 def matrix_transpose(matrix):
+    """
+    Transposes a matrix.
+
+    Parameters:
+    matrix (list of lists): The matrix to be transposed.
+
+    Returns:
+    list of lists: The transposed matrix, where rows become columns and vice versa.
+    """
     return [list(row) for row in zip(*matrix)]
