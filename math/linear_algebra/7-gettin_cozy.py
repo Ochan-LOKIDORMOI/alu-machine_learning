3229da492dff772cb35f787a7007a4c3aafdba4a
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-19 07:40:37 -0700

    updated

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """
-    Concatenates two matrices along a specific axis.
+    Concatenate two 2D matrices along a specified axis.
+
+    This function takes two 2D matrices (represented as lists of lists) and
+    concatenates them along the specified axis. If the axis is 0, the matrices
+    are concatenated along the rows. If the axis is 1, the matrices are
+    concatenated along the columns.
 
     Parameters:
-    mat1 (list of lists): The first matrix.
-    mat2 (list of lists): The second matrix.
+    mat1 (list of lists): The first matrix to be concatenated.
+    mat2 (list of lists): The second matrix to be concatenated.
     axis (int, optional): The axis along which to concatenate the matrices.
-        If axis is 0, concatenates along the rows.
-        If axis is 1, concatenates along the columns. Defaults to 0.
+        If axis is 0, the matrices are concatenated along the rows.
+        If axis is 1, the matrices are concatenated along the columns.
+        Defaults to 0.
 
     Returns:
-    A new matrix resulting from concatenating mat1 and mat2
-    along the specified axis. Returns None if concatenation is not possible.
+    list of lists: A new matrix resulting from the concatenation of mat1 and
+        mat2 along the specified axis. Returns None if the concatenation is
+        not possible (e.g., if the matrices have different dimensions along
+        the non-concatenation axis).
     """
     if axis == 0:
         # Concatenate along the rows
