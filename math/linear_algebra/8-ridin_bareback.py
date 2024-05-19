f568a64118914b08dbdfb85ab028b605be5c119e
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-19 07:54:12 -0700

    Ridin’ Bareback

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -9,14 +9,14 @@ def mat_mul(mat1, mat2):
     Returns
     m_array: multiplied matrices
     """
-    m_array = [
-        [0 for _ in range(len(mat2[0]))]
-        for _ in range(len(mat1))
-    ]
-
+    if len(mat1[0]) != len(mat2):
+        return None
+    
+    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+    
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
-                m_array[i][j] += mat1[i][k] * mat2[k][j]
-
-    return m_array
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    
+    return result
