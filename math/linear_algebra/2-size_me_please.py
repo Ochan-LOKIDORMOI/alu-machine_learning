1d5113f21339dbdb2774ba817c83f1cab8fe9add
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-19 05:40:36 -0700

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -8,8 +8,8 @@ def matrix_shape(matrix):
     return shape
 
 mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))  # Expected output: [2, 2]
+print(matrix_shape(mat1))
 
 mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
         [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))  # Expected output: [2, 3, 5]
+print(matrix_shape(mat2)
