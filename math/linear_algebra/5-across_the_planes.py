45734b08e3b21af236f761706d38fb4642f4b579
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-19 06:53:16 -0700

    Across The Planes

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -14,9 +14,10 @@ def add_matrices2D(mat1, mat2):
 
     Returns:
     lists or None: A new matrix containing the result of adding corresponding
-    elements of mat1 and mat2.
+    elements of mat1 and mat2. Returns None if the matrices are not of the same shape.
     """
-    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+    if (len(mat1) != len(mat2) or
+            any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2))):
         return None
     return [
         [elem1 + elem2 for elem1, elem2 in zip(row1, row2)]
