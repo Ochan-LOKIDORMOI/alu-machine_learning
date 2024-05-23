e8483724e1043899db49322e803d5053a49c60f5
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-05-23 10:30:52 -0700

    Adjucate

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -1,81 +1,127 @@
 #!/usr/bin/env python3
 """
-Module for calculating the adjugate matrix of a given matrix.
+Defines function that calculates the adjugate matrix of a matrix
 """
-
-
-def determinant(matrix):
-    """
-    Helper function to calculate the determinant of a matrix.
-
-    Args:
-        matrix (list of lists): The matrix to calculate the determinant for.
-
-    Returns:
-        int or float: The determinant of the matrix.
-    """
-    if len(matrix) == 1:
-        return matrix[0][0]
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
-    det = 0
-    for col in range(len(matrix)):
-        submatrix = [row[:col] + row[col + 1:] for row in matrix[1:]]
-        det += ((-1) ** col) * matrix[0][col] * determinant(submatrix)
-    return det
-
-
-def cofactor(matrix):
-    """
-    Calculates the cofactor matrix of a matrix.
-
-    Args:
-        matrix (list of lists): The matrix whose cofactor matrix
-        should be calculated.
-
-    Returns:
-        list of lists: The cofactor matrix of the given matrix.
-    """
-    if not isinstance(matrix, list) or not all(isinstance(row, list)
-                                               for row in matrix):
-        raise TypeError("matrix must be a list of lists")
-    if len(matrix) == 0 or any(len(row) != len(matrix) for row in matrix):
-        raise ValueError("matrix must be a non-empty square matrix")
-
-    if len(matrix) == 1 and len(matrix[0]) == 1:  # Correct handling for single-element matrix
-        return [[1]]
-
-    minor_matrix = []
-    for i in range(len(matrix)):
-        minor_row = []
-        for j in range(len(matrix)):
-            submatrix = [row[:j] + row[j + 1:] for k, row in enumerate(matrix)
-                         if k != i]
-            cofactor_value = ((-1) ** (i + j)) * determinant(submatrix)
-            minor_row.append(cofactor_value)
-        minor_matrix.append(minor_row)
-
-    return minor_matrix
 
 
 def adjugate(matrix):
     """
-    Calculates the adjugate matrix of a matrix.
+    Calculates the adjugate matrix of a matrix
 
-    Args:
-        matrix (list of lists): The matrix whose adjugate matrix
-        should be calculated.
+    parameters:
+        matrix [list of lists]:
+            matrix whose adjugate matrix should be calculated
 
-    Returns:
-        list of lists: The adjugate matrix of the given matrix.
-
-    Raises:
-        TypeError: If matrix is not a list of lists.
-        ValueError: If matrix is not square or is empty.
+    returns:
+        the adjugate matrix of matrix
     """
-    cofactor_matrix = cofactor(matrix)
-
-    # Transpose the cofactor matrix to get the adjugate matrix
-    adjugate_matrix = list(map(list, zip(*cofactor_matrix)))
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a list of lists")
+    matrix_size = len(matrix)
+    if matrix_size == 0:
+        raise TypeError("matrix must be a list of lists")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(row) != matrix_size:
+            raise ValueError("matrix must be a non-empty square matrix")
+    if matrix_size == 1:
+        return [[1]]
+    multiplier = 1
+    cofactor_matrix = []
+    for row_idx in range(matrix_size):
+        cofactor_row = []
+        for column_idx in range(matrix_size):
+            sub_matrix = []
+            for row in range(matrix_size):
+                if row == row_idx:
+                    continue
+                new_row = []
+                for column in range(matrix_size):
+                    if column == column_idx:
+                        continue
+                    new_row.append(matrix[row][column])
+                sub_matrix.append(new_row)
+            cofactor_row.append(multiplier * determinant(sub_matrix))
+            multiplier *= -1
+        cofactor_matrix.append(cofactor_row)
+        if matrix_size % 2 == 0:
+            multiplier *= -1
+    adjugate_matrix = transpose(cofactor_matrix)
     return adjugate_matrix
+
+
+def determinant(matrix):
+    """
+    Calculates the determinant of a matrix
+
+    parameters:
+        matrix [list of lists]:
+            matrix whose determinant should be calculated
+
+    returns:
+        the determinant of matrix
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a list of lists")
+    matrix_size = len(matrix)
+    if matrix_size == 0:
+        raise TypeError("matrix must be a list of lists")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(row) == 0 and matrix_size == 1:
+            return 1
+        if len(row) != matrix_size:
+            raise ValueError("matrix must be a square matrix")
+    if matrix_size == 1:
+        return matrix[0][0]
+    if matrix_size == 2:
+        a = matrix[0][0]
+        b = matrix[0][1]
+        c = matrix[1][0]
+        d = matrix[1][1]
+        return ((a * d) - (b * c))
+    multiplier = 1
+    det = 0
+    for i in range(matrix_size):
+        element = matrix[0][i]
+        sub_matrix = []
+        for row in range(matrix_size):
+            if row == 0:
+                continue
+            new_row = []
+            for column in range(matrix_size):
+                if column == i:
+                    continue
+                new_row.append(matrix[row][column])
+            sub_matrix.append(new_row)
+        det += (element * multiplier * determinant(sub_matrix))
+        multiplier *= -1
+    return det
+
+
+def transpose(matrix):
+    """
+    Calculates the transpose of a square matrix
+    Matrix is assumed to be valid and square
+        based on previous type and value checks
+        from prior functions in which transpose is called
+
+    parameters:
+        matrix [list of lists]:
+            matrix whose transpose should be calculated
+
+    returns:
+        the transpose of matrix
+    """
+    matrix_size = len(matrix)
+    transpose_matrix = []
+    for i in range(matrix_size):
+        t_row = []
+        for row in range(matrix_size):
+            for column in range(matrix_size):
+                if column == i:
+                    t_row.append(matrix[row][column])
+        transpose_matrix.append(t_row)
+    return transpose_matrix
