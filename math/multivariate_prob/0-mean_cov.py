54b1e827b5f333ad91701366ff4282d00bb3ff11
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-06-14 13:54:59 -0700

    Mean and Covariance

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+"""mean and covariance"""
 
-if __name__ == '__main__':
-    import numpy as np
-    mean_cov = __import__('0-mean_cov').mean_cov
 
-    np.random.seed(0)
-    X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000)
-    mean, cov = mean_cov(X)
-    print(mean)
-    print(cov)
+import numpy as np
+
+
+def mean_cov(X):
+    """Calculating the covariance of dataset
+
+    X is a numpy.ndarray of shape (n, d) containing the data set:
+        n is the number of data points
+        d is the number of dimensions in each data point
+    If X is not a 2D numpy.ndarray, raise a TypeError with the message X
+    must be a 2D numpy.ndarray
+    """
+    if len(X.shape) != 2 or type(X) is not np.ndarray:
+        raise TypeError('X must be a 2D numpy.ndarray')
+    data, dim = X.shape
+    if data < 2:
+        raise ValueError('X must contain multiple data points')
+    mean = np.mean(X, axis=0).reshape(1, dim)
+    x = X - mean
+    cov = (x.T @ x) / (data - 1)
+    return mean, cov
