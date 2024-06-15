5c7637798bfd9b5cb69777f01b4993e7d5a789d2
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-06-14 19:24:10 -0700

    PDF

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Creates the class MultiNormal"""
+"""This script Creates the class MultiNormal"""
 
 
 import numpy as np
@@ -7,7 +7,7 @@ import numpy as np
 
 class MultiNormal:
     """
-    Represents a Multivariate Normal distribution
+    Multivariate Normal distribution
     """
 
     def __init__(self, data):
@@ -28,3 +28,26 @@ class MultiNormal:
         self.mean = data.mean(axis=1).reshape(d, 1)
         X_mean = data - self.mean
         self.cov = (X_mean @ X_mean.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        This script calculates the PDF
+        :param x: numpy.ndarray of shape (d, 1) containing the data point whose
+        PDF should be calculated
+            d is the number of dimensions of the Multinomial instance
+        :return: the value of the PDF
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        first = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det))
+        second = np.dot((x - self.mean).T, inv)
+        third = np.dot(second, (x - self.mean) / -2)
+        pdf = first * np.exp(third)
+
+        return pdf[0][0]
