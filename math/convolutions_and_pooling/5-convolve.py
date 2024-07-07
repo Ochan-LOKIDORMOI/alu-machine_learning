690fa789c3c7126067dbe153169a1c71cd06049c
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-07-07 14:39:09 -0700

    Multiple Kernels

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -32,34 +32,33 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
 
     Returns: a numpy.ndarray containing the convolved images"""
     m, h, w, c = images.shape
-    kh, kw = kernels.shape[:2]
+    kh, kw, _, nc = kernels.shape
     sh, sw = stride
 
-    img = images
-
     if padding == 'same':
-        ph, pw = int(((kh - 1) / 2) / sh), int(((kw - 1) / 2) / sw)
-        convolveImage = np.zeros(shape=images.shape)
-        img = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                     mode='constant',
-                     constant_values=0)
-    if padding == 'valid':
-        h, w = int((h - kh) / sh + 1), int((w - kw) / sw + 1)
-        convolveImage = np.zeros(shape=(m, h, w, c))
-    if type(padding) is tuple:
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
+        ph = pw = 0
+    else:
         ph, pw = padding
-        convolveImage = np.zeros(shape=images.shape)
-        img = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                     mode='constant',
-                     constant_values=0)
 
-    for z in range(c):
-        for y in range(h):
-            for x in range(w):
-                y0 = y * sh
-                y1 = y0 + kh
-                x0 = x * sw
-                x1 = x0 + kw
-                convolveImage[:, y, x, z] = np.sum(img[:, y0:y1, x0:x1] * kernels[..., z],
-                                                axis=(1, 2, 3))
-    return convolveImage
+    padded_h = h + 2 * ph
+    padded_w = w + 2 * pw
+
+    output_h = (padded_h - kh) // sh + 1
+    output_w = (padded_w - kw) // sw + 1
+
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+
+    output = np.zeros((m, output_h, output_w, nc))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            for k in range(nc):
+                output[:, i, j, k] = np.sum(
+                    padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :] * kernels[:, :, :, k],
+                    axis=(1, 2, 3)
+                )
+
+    return output
