72462b864d4beb1564c1788df4e8b26695d98606
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-07-21 04:44:03 +0200

    Updated

--- math/summative/FlutterApp/lib/main.dart
+++ math/summative/FlutterApp/lib/main.dart
@@ -3,24 +3,28 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FastAPI Predictor',
+      title: 'Students Preformance App Predictor',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -60,62 +64,62 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FastAPI Predictor'),
+        title: const Text('Students Preformance App Predictor'),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Enter the details below',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 25),
             _buildTextField(
               controller: hoursStudiedController,
               labelText: 'Hours Studied',
               icon: Icons.access_time,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 25),
             _buildTextField(
               controller: previousScoresController,
               labelText: 'Previous Scores',
               icon: Icons.score,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 25),
             _buildTextField(
               controller: sleepHoursController,
               labelText: 'Sleep Hours',
               icon: Icons.bedtime,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 25),
             _buildTextField(
               controller: samplePapersController,
               labelText: 'Sample Question Papers Practiced',
               icon: Icons.book,
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
             ElevatedButton(
               onPressed: predictPerformance,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
-                child: Text(
-                  'Predict Performance Index',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
+                backgroundColor: Color.fromARGB(255, 64, 169, 245),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
               ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                child: Text(
+                  'Predict Performance Index',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Text(
               result.isEmpty ? 'Enter values to get the prediction' : 'Performance Index: $result',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.teal),
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.teal),
             ),
           ],
         ),
