258b83a7d7a3baab54a0fa0c316eb372ed499e3c
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-12 20:11:28 +0200

    DeepNeuralNetwork Forward Propagation

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -11,44 +11,60 @@ class DeepNeuralNetwork:
     def __init__(self, nx, layers):
         """
         Initializes the deep neural network
-        
+
         nx: number of input features
         layers: list representing the number of nodes in each layer
         """
-        if not isinstance(nx, int) or nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0 or not all(isinstance(i, int) and i > 0 for i in layers):
-            raise TypeError("layers must be a list of positive integers")
-
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
-        for i in range(self.L):
-            if i == 0:
-                self.weights['W1'] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for n in range(len(layers)):
+            if layers[n] < 1 or type(layers[n]) is not int:
+                raise TypeError('layers must be a list of positive integers')
+            w = 'W' + str(n + 1)
+            b = 'b' + str(n + 1)
+            if n == 0:
+                layerPrev = nx
             else:
-                self.weights['W{}'.format(i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-            self.weights['b{}'.format(i + 1)] = np.zeros((layers[i], 1))
+                layerPrev = layers[n - 1]
+            sqrt = np.sqrt(2 / layerPrev)
+            self.__weights[w] = np.random.randn(layers[n], layerPrev) * sqrt
+            self.__weights[b] = np.zeros(shape=(layers[n], 1))
+
+    @property
+    def L(self):
+        """Getter function of private attribute n"""
+        return self.__L
+
+    @property
+    def weights(self):
+        """Getter function of private attribute weights"""
+        return self.__weights
+
+    @property
+    def cache(self):
+        """Getter function of private attribute cache"""
+        return self.__cache
 
     def forward_prop(self, X):
-        """
-        Calculates the forward propagation of the deep neural network
-        
-        X: input data, numpy.ndarray of shape (nx, m)
-        """
-        self.cache['A0'] = X
-        for i in range(1, self.L + 1):
-            Z = np.dot(self.weights['W{}'.format(i)], self.cache['A{}'.format(i - 1)]) + self.weights['b{}'.format(i)]
-            self.cache['A{}'.format(i)] = 1 / (1 + np.exp(-Z))
-        return self.cache['A{}'.format(self.L)], self.cache
+        """Function that calculates the forward propagation of the
+        neural network
 
-    def cost(self, Y, A):
-        """
-        Calculates the cost of the model using logistic regression
-        
-        Y: numpy.ndarray with shape (1, m) containing the correct labels
-        A: numpy.ndarray with shape (1, m) containing the activated output
-        """
-        m = Y.shape[1]
-        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
-        return cost
+        X: is a numpy.ndarray with shape (nx, m) that contains the input data
+
+        Return: output of the neural network and the cache, respectively"""
+        self.__cache['A0'] = X
+        for n in range(self.L):
+            a = 'A' + str(n)
+            aNext = 'A' + str(n + 1)
+            w = 'W' + str(n + 1)
+            b = 'b' + str(n + 1)
+            x = np.matmul(self.weights[w], self.cache[a]) + self.weights[b]
+            self.__cache[aNext] = 1 / (1 + np.e**(-x))
+        return self.__cache[aNext], self.cache
