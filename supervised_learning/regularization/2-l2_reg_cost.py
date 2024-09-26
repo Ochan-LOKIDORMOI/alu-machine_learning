bcb97470b84753f91845abd906787b48d0192cb6
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-26 19:53:13 +0200

    L2 Regularization Cost

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" L2 Regularization Cost"""
+
+import numpy as np
+
+
+def l2_reg_cost(cost):
+    """ calculates the cost of a neural network with L2 regularization
+    Args:
+        cost: cost of the network without L2 regularization
+    Returns: cost of the network accounting for L2 regularization
+    """
+    return cost
