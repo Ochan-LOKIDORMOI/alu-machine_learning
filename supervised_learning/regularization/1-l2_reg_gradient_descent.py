1b2db16d4882c100c16a2dc155270fb075853f85
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-26 18:22:19 +0200

    Gradient Descent with L2 Regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-"""Gradient Descent with L2 Regularization"""
+"""Gradient descent with L2 regularization"""
+
 
 import numpy as np
 
+
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """ updates the weights and biases of a neural network using gradient
-        descent with L2 regularization
-    """
+    """ that updates the weights and biases of a neural network using gradient
+    descent with L2 regularization:
+
+    Y: is a one-hot numpy.ndarray of shape (classes, m) that contains the
+    correct labels for the data
+        classes: is the number of classes
+        m: is the number of data points
+    weights: is a dictionary of the weights and biases of the neural network
+    cache: is a dictionary of the outputs of each layer of the neural network
+    alpha: is the learning rate
+    lambtha: is the L2 regularization parameter
+    L: is the number of layers of the network"""
     m = Y.shape[1]
     la = L
     a = 'A' + str(la)
