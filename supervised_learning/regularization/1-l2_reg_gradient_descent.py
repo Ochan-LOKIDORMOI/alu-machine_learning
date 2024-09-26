5f6b1a988a0efbaec1706ae4432d2ae92ba33ff4
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-26 18:20:29 +0200

    Gradient Descent with L2 Regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -2,20 +2,33 @@
 """Gradient Descent with L2 Regularization"""
 
 import numpy as np
+
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """ updates the weights and biases of a neural network using gradient
         descent with L2 regularization
     """
     m = Y.shape[1]
-    for i in range(L, 0, -1):
-        A = cache['A' + str(i)]
-        if i == L:
-            dz = A - Y
-        else:
-            dz = da * (1 - A**2)
-        da = dz
-        dW = (1/m) * np.matmul(dz, cache['A' + str(i - 1)].T) + \
-             (lambtha/m) * weights['W' + str(i)]
-        db = (1/m) * np.sum(dz, axis=1, keepdims=True)
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
+    la = L
+    a = 'A' + str(la)
+    W = 'W' + str(la)
+    b = 'b' + str(la)
+    dz = cache[a] - Y
+    dw = (np.dot(cache['A' + str(la - 1)], dz.T) / m).T
+    dw = dw + (lambtha / m) * weights[W]
+    db = np.sum(dz, axis=1, keepdims=True) / m
+    weights[W] = weights[W] - alpha * dw
+    weights[b] = weights[b] - alpha * db
+
+    for la in range(L - 1, 0, -1):
+        a = 'A' + str(la)
+        W = 'W' + str(la)
+        b = 'b' + str(la)
+        wNext = 'W' + str(la + 1)
+        aNext = 'A' + str(la - 1)
+        g = cache[a] * (1 - cache[a])
+        dz = np.dot(weights[wNext].T, dz) * g
+        dw = (np.dot(cache[aNext], dz.T) / m).T + ((lambtha / m) * weights[W])
+        db = np.sum(dz, axis=1, keepdims=True) / m
+
+        weights[W] = weights[W] - alpha * dw
+        weights[b] = weights[b] - alpha * db
