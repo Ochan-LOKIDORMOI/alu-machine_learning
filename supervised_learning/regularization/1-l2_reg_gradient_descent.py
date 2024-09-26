a9367cc1ad4b76a5c2580e7aef281860d532acfe
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-26 18:40:21 +0200

    Gradient Descent with L2 Regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -6,24 +6,32 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """ updates the weights and biases of a neural network using gradient
-        descent with L2 regularization
-        Y: (numpy.ndarray) one-hot encoded labels of the data
-        weights: (dict) weights and biases of the network
-        cache: (dict) output of each layer
-        alpha: (float) learning rate
-        lambtha: (float) regularization parameter
-        L: (int) number of layers in the network
-        Returns: None
+    """
+    Updates the weights and biases of a neural network using gradient descent with L2 regularization.
+
+    Parameters:
+    - Y: numpy.ndarray of shape (classes, m) containing the correct labels for the data
+    - weights: dictionary of the weights and biases of the neural network
+    - cache: dictionary of the outputs of each layer of the neural network
+    - alpha: learning rate
+    - lambtha: L2 regularization parameter
+    - L: number of layers of the network
     """
     m = Y.shape[1]
-    dz = cache["A" + str(L)] - Y
+    dZ = cache["A" + str(L)] - Y  # dZ for the last layer (softmax)
+
     for i in range(L, 0, -1):
-        A = cache["A" + str(i - 1)]
+        A_prev = cache["A" + str(i - 1)] if i > 1 else cache["A0"]
         W = weights["W" + str(i)]
         b = weights["b" + str(i)]
-        dw = (1 / m) * np.matmul(dz, A.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        dz = np.matmul(W.T, dz) * (A * (1 - A))
-        weights["W" + str(i)] = weights["W" + str(i)] - alpha * dw
-        weights["b" + str(i)] = weights["b" + str(i)] - alpha * db
+
+        # Compute gradients for weights and biases
+        dW = (np.dot(dZ, A_prev.T) / m) + (lambtha / m) * W
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+
+        if i > 1:
+            # Compute dZ for the previous layer (before softmax/tanh)
+            dA_prev = np.dot(W.T, dZ)
+            dZ = dA_prev * (1 - A_prev ** 2)
+        weights["W" + str(i)] -= alpha * dW
+        weights["b" + str(i)] -= alpha * db
