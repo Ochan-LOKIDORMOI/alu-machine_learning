2ab8569b73316a5ccb380f8864194e14bc39f81a
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-29 17:07:24 +0200

    RMSProp Upgraded

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""RMSProp Upgraded"""
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """creates the training operation for a neural network in tensorflow
+    using the RMSProp optimization algorithm"""
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    train = optimizer.minimize(loss)
+    return train
