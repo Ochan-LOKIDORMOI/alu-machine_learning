c8792584de632e1e67ba1ec283bc68d145f015d3
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-29 15:43:01 +0200

    Normalization Constants

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def normalization_constants(X):
-    """Calculates the normalization (standardization)
-    constants of a matrix
-    """
-    return tf.math.reduce_mean(X, axis=0), tf.math.reduce_std(X, axis= 0)
+    """Calculates the normalization (standardization) constants of a matrix"""
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    return
