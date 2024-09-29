33e9cde342963b9f86ca23bc5a51845da6048a67
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-29 16:05:38 +0200

    Mini-batch

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,41 +1,82 @@
 #!/usr/bin/env python3
-"""Mini-Batch"""
+"""Shuffle data"""
+
 
 import tensorflow as tf
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
-                     epochs=5, load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
-    """
-    Function that trains a loaded neural network
-    model using mini-batch gradient descent
-    """
+                     epochs=5, load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """Function that trains a loaded neural network model using mini-batch
+    gradient descent:
+
+    X_train: is a numpy.ndarray of shape (m, 784) containing the training data
+        m is the number of data points
+        784 is the number of input features
+    Y_train: is a one-hot numpy.ndarray of shape (m, 10) containing the
+    training labels
+        10 is the number of classes the model should classify
+    X_valid: is a numpy.ndarray of shape (m, 784) containing the validation
+    data
+    Y_valid: is a one-hot numpy.ndarray of shape (m, 10) containing the
+    validation labels
+    batch_size: is the number of data points in a batch
+    epochs: is the number of times the training should pass through the whole
+    dataset
+    load_path: is the path from which to load the model
+    save_path: is the path to where the model should be saved after training
+
+    Returns: the path where the model was saved"""
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(load_path + ".meta")
-        saver.restore(sess, load_path)
-        x = tf.get_collection("x")[0]
+        metaGraph = tf.train.import_meta_graph(load_path + ".meta")
+        metaGraph.restore(sess, load_path)
+        x = tf.get_collection('x')[0]
         y = tf.get_collection("y")[0]
-        loss = tf.get_collection("loss")[0]
-        accuracy = tf.get_collection("accuracy")[0]
-        train_op = tf.get_collection("train_op")[0]
+        loss = tf.get_collection('loss')[0]
+        acc = tf.get_collection('accuracy')[0]
+        train_op = tf.get_collection('train_op')[0]
+        feed_train = {x: X_train, y: Y_train}
+        feed_valid = {x: X_valid, y: Y_valid}
+
+        batch_float = X_train.shape[0] / batch_size
+        batch_int = int(batch_float)
+
+        extra = False
+        # conditional that identify if is necessary an extra step
+        if (batch_float > batch_int):
+            batch_int = int(batch_float) + 1
+            extra = True
+
         for epoch in range(epochs + 1):
-            t_loss = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            t_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            v_loss = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-            v_accuracy = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+            cost_train = sess.run(loss, feed_train)
+            acc_train = sess.run(acc, feed_train)
+            cost_valid = sess.run(loss, feed_valid)
+            acc_valid = sess.run(acc, feed_valid)
+
             print("After {} epochs:".format(epoch))
-            print("\tTraining Cost: {}".format(t_loss))
-            print("\tTraining Accuracy: {}".format(t_accuracy))
-            print("\tValidation Cost: {}".format(v_loss))
-            print("\tValidation Accuracy: {}".format(v_accuracy))
-            if epoch < epochs:
-                for i in range(0, X_train.shape[0], batch_size):
-                    X_batch = X_train[i:i + batch_size]
-                    Y_batch = Y_train[i:i + batch_size]
-                    sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
-                    if i != 0 and (i / batch_size + 1) % 100 == 0:
-                        b_loss = sess.run(loss, feed_dict={x: X_batch, y: Y_batch})
-                        b_accuracy = sess.run(accuracy, feed_dict={x: X_batch, y: Y_batch})
-                        print("\tStep {}:".format(i // batch_size + 1))
-                        print("\t\tCost: {}".format(b_loss))
-                        print("\t\tAccuracy: {}".format(b_accuracy))
-        return saver.save(sess, save_path)
+            print("\tTraining Cost: {}".format(cost_train))
+            print("\tTraining Accuracy: {}".format(acc_train))
+            print("\tValidation Cost: {}".format(cost_valid))
+            print("\tValidation Accuracy: {}".format(acc_valid))
+
+            if (epoch < epochs):
+                X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
+                for i in range(batch_int):
+                    start = i * batch_size
+                    end = batch_size * (i + 1)
+                    if i == batch_int - 1 and extra:
+                        end = int(batch_size * (
+                            i + batch_float - batch_int + 1))
+                    feed_batch = {x: X_shuffled[start: end],
+                                  y: Y_shuffled[start: end]}
+                    sess.run(train_op, feed_batch)
+                    if i != 0 and (i + 1) % 100 == 0:
+                        print("\tStep {}:".format(i + 1))
+                        cost_batch = sess.run(loss, feed_batch)
+                        print("\t\tCost: {}".format(cost_batch))
+                        acc_batch = sess.run(acc, feed_batch)
+                        print("\t\tAccuracy: {}".format(acc_batch))
+        save_path = metaGraph.save(sess, save_path)
+    return save_path
