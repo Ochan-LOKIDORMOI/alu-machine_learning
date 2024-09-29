5052bfc4c65f49bc1b118b25533889fa27633245
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-09-29 17:23:23 +0200

    RMSProp Upgraded

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -9,5 +9,3 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     using the RMSProp optimization algorithm"""
     return tf.train.RMSPropOptimizer(alpha, beta2,
                                      epsilon=epsilon).minimize(loss)
-
-
