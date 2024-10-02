8d6f553342a7dae5ecccd26181b0d2dfe8f9df7b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-02 23:06:32 +0200

    Create Confusion

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -6,8 +6,8 @@ import numpy as np
 
 def create_confusion_matrix(labels, logits, classes):
     """creates a confusion matrix"""
-    m, classes = labels.shape
     confusion = np.zeros((classes, classes))
-    for i in range(m):
-        confusion[np.argmax(labels[i]), np.argmax(logits[i])] += 1
+    for i in range(len(labels)):
+        confusion[labels[i]][np.argmax(logits[i])] += 1
     return confusion
+
