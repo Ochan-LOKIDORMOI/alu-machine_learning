543cfaddd2a3ee39beef29d17b2b9440eef60776
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-03 00:08:46 +0200

    error analysis

--- supervised_learning/error_analysis/5-error_handling.py
+++ supervised_learning/error_analysis/5-error_handling.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-"""Error Handling"""
-
-error_analysis = {
-	"High Bias, High Variance": ["C", "E"],
-	"High Bias, Low Variance": ["B", "D"],
-	"Low Bias, High Variance": ["C", "E"],
-	"Low Bias, Low Variance": ["F"]
-}
+A,B,D
+A,B,D
+B,C,E
+F
