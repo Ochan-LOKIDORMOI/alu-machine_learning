a7e8399bf4b55e786d6be02cda218f47133009f4
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-19 02:19:04 +0300

    From File

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,10 +1,17 @@
-#!/usr/bin/env python
-
-"From File"
+#!/usr/bin/env python3
+"""
+Module to read a csv file
+"""
 
 import pandas as pd
 
 
 def from_file(filename, delimiter):
-    # Use pandas' read_csv to load the data with the specified delimiter
+    """
+    Function to read a csv file
+    Args:
+        filename: the file to read
+        delimiter: the column separator
+    Returns: the data from the file as a pd.DataFrame
+    """
     return pd.read_csv(filename, delimiter=delimiter)
