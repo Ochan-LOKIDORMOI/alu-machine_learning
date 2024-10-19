17a6c2291628b04a4cab83ac260bd498c04596c1
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-19 23:17:24 +0300

    Slice

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+# Load the dataset
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
+
+# Print the last 5 rows
+print(df.tail())
