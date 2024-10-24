55ab8d6955a6fd45b78b9f54a1f2425f5ced3d5a
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-24 14:39:25 +0200

    Where I am?

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Sentience API
+"""
+
+import requests
+
+def sentientPlanets():
+    base_url = "https://swapi-api.alx-tools.com/species/"
+    planets = set()
+    next_page = base_url
+
+    while next_page:
+        response = requests.get(next_page)
+        if response.status_code != 200:
+            break
+
+        data = response.json()
+        species_list = data['results']
+        
+        for species in species_list:
+            if species.get('designation') == 'sentient':
+                homeworld_url = species.get('homeworld')
+                if homeworld_url:
+                    # Get the homeworld name
+                    homeworld_response = requests.get(homeworld_url)
+                    if homeworld_response.status_code == 200:
+                        homeworld_data = homeworld_response.json()
+                        planets.add(homeworld_data.get('name', 'unknown'))
+                    else:
+                        planets.add('unknown')
+                else:
+                    planets.add('unknown')
+        
+        next_page = data.get('next')
+
+    return sorted(planets)
