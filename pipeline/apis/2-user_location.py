2085e0548d2059a21b5268bdc5e647bc164e05fd
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-25 21:58:22 +0200

    Rate me is you can!

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python3
-
-
-""" Return list of ships"""
-
-import requests
 import sys
-import time
+import requests
+from datetime import datetime
 
+def fetch_user_location(url):
+    try:
+        response = requests.get(url)
+        
+        # Check if the user exists
+        if response.status_code == 404:
+            print("Not found")
+            return
+        
+        # Handle rate limit
+        elif response.status_code == 403:
+            reset_timestamp = int(response.headers.get("X-Ratelimit-Reset", 0))
+            reset_time = datetime.fromtimestamp(reset_timestamp)
+            minutes_remaining = int((reset_time - datetime.now()).total_seconds() / 60)
+            print(f"Reset in {minutes_remaining} min")
+            return
 
-if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
+        # Successful response
+        elif response.status_code == 200:
+            user_data = response.json()
+            location = user_data.get("location", "Location not available")
+            print(location)
+            return
 
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print("Reset in {} min".format(diff))
-        # get remaining rate
+        # Other status codes
+        else:
+            print("An error occurred")
 
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <user_api_url>")
+    else:
+        user_url = sys.argv[1]
+        fetch_user_location(user_url)
