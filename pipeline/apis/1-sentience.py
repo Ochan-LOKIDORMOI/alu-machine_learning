cca3b97049b621ff83a79ea73ae022a793a7717c
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-25 21:36:17 +0200

    Where I am?

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,40 +1,26 @@
 #!/usr/bin/env python3
-"""
-This module contains the function to retrieve the home planets
-of all sentient species from the SWAPI API.
-"""
-
+"""Fetch Home Planets of Sentient Species"""
 import requests
 
 
-def sentientPlanets():
-    """
-    Fetches and returns a list of names of home planets for all sentient species
-    from the SWAPI API.
-
-    The function handles pagination to collect all species marked as 'sentient' and
-    retrieves the corresponding homeworld name for each. If a homeworld is not specified
-    or if the API call fails, 'unknown' is used as a placeholder.
-
-    Returns:
-        list: A sorted list of unique planet names of all sentient species.
-    """
-    base_url = "https://swapi-api.alx-tools.com/species/"
-    planets = set()
-    next_page = base_url
+def get_sentient_species_homeworlds():
+    """Retrieve names of home planets for all sentient species."""
+    api_url = "https://swapi-api.hbtn.io/api/species"
+    homeworlds = []
     
-    while next_page:
-        response = requests.get(next_page)
-        if response.status_code != 200:
+    response = requests.get(api_url)
+    while response.status_code == 200:
+        for species in response.json()["results"]:
+            homeworld_url = species["homeworld"]
+            if homeworld_url:
+                homeworld_response = requests.get(homeworld_url)
+                homeworlds.append(homeworld_response.json()["name"])
+        
+        # Fetch next page of species
+        next_page = response.json().get("next")
+        if next_page:
+            response = requests.get(next_page)
+        else:
             break
-        data = response.json()
-        for species in data['results']:
-            if species['designation'] == 'sentient':
-                homeworld = species['homeworld']
-                if homeworld:
-                    planet = requests.get(homeworld).json()['name']
-                else:
-                    planet = 'unknown'
-                planets.add(planet)
-    next_page = data['next']
-    return sorted(list(planets))
+            
+    return homeworlds
