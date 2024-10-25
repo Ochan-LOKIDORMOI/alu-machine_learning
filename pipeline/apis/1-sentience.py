3a0dedaf79a8c58b4fd7cd7fa22f1908992e03da
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-25 21:42:09 +0200

    Where I am?

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python3
-"""Fetch Home Planets of Sentient Species"""
+"""Pipeline Api"""
 import requests
 
 
-def get_sentient_species_homeworlds():
-    """Retrieve names of home planets for all sentient species."""
-    api_url = "https://swapi-api.hbtn.io/api/species"
-    homeworlds = []
-    
-    response = requests.get(api_url)
-    while response.status_code == 200:
-        for species in response.json()["results"]:
-            homeworld_url = species["homeworld"]
-            if homeworld_url:
-                homeworld_response = requests.get(homeworld_url)
-                homeworlds.append(homeworld_response.json()["name"])
-        
-        # Fetch next page of species
-        next_page = response.json().get("next")
-        if next_page:
-            response = requests.get(next_page)
-        else:
+def sentientPlanets():
+    """returns the list of names of the home planets of all sentient species"""
+    url = "https://swapi-api.hbtn.io/api/species"
+    r = requests.get(url)
+    world_list = []
+    while r.status_code == 200:
+        for species in r.json()["results"]:
+            url = species["homeworld"]
+            if url is not None:
+                ur = requests.get(url)
+                world_list.append(ur.json()["name"])
+        try:
+            r = requests.get(r.json()["next"])
+        except Exception:
             break
-            
-    return homeworlds
+    return world_list
