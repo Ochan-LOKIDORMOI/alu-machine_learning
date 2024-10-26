e687734001b40ea13ec6b458481be7bfad36b2c2
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-26 18:17:22 +0200

    What will be next?

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -4,13 +4,11 @@ import requests
 from datetime import datetime
 
 
-
 if __name__ == '__main__':
     """pipeline api"""
     url = "https://api.spacexdata.com/v4/launches/upcoming"
     r = requests.get(url)
     recent = 0
-
 
     for dic in r.json():
         new = int(dic["date_unix"])
@@ -19,12 +17,9 @@ if __name__ == '__main__':
             launch_name = dic["name"]
             date = dic["date_local"]
             rocket_id = dic["rocket"]
-
-
     rurl = "https://api.spacexdata.com/v4/rockets/" + rocket_id
     rocket_data = requests.get(rurl).json()
     rocket_name = rocket_data["name"]
-
 
     launchpad_id = dic["launchpad"]
     lurl = "https://api.spacexdata.com/v4/launchpads/" + launchpad_id
@@ -33,9 +28,7 @@ if __name__ == '__main__':
     launchpad_name = launchpad_data["name"]
     launchpad_local = launchpad_data["locality"]
 
-
     string = "{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
     launchpad_name, launchpad_local)
-
 
     print(string)
