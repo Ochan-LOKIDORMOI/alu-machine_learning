124ea4bfde08b086c0f6ffd69f6c74f7800efe74
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-31 09:50:33 +0200

    Rotten tomatoes

---
+++ pipeline/databases/11-rating_shows.sql
@@ -0,0 +1,7 @@
+-- The script to list all shows with their rating sum, sorted in descending order by the rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+FROM tv_shows
+JOIN tv_show_ratings 
+ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
