4968743d9a9cac20fe03f882302b0bb5a898925d
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-10-31 09:38:18 +0200

    Number of shows by genre

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
--- A script to list all genres and the number of shows linked to each genre,
---ordered by the number of shows in descending order
+-- A script to list all genres and the number of shows linked to each genre,ordered by the number of shows in descending order
 SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
