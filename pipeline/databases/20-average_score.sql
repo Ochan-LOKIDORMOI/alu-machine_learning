756e4912fa3c35c92bc82f44519afb5d7e2ae96a
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-11-01 23:49:39 +0200

    Average score

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,19 +1,21 @@
 -- A SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id_param INT
+)
 BEGIN
-    DECLARE avg_score FLOAT;
-
-    -- Calculate the average score for the specified user
-    SELECT AVG(score) INTO avg_score
+    DECLARE user_avg_score FLOAT;
+    
+    -- Calculate average score for the user
+    SELECT AVG(score) INTO user_avg_score
     FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the user's average_score in the users table
+    WHERE user_id = user_id_param;
+    
+    -- Update the average_score column in the users table
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    SET average_score = user_avg_score
+    WHERE id = user_id_param;
 END //
 
 DELIMITER ;
