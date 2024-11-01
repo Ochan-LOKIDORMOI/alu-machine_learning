a0124a810ebfc7a779989eac421d085df7287445
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-11-01 23:29:56 +0200

    updated

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -2,7 +2,6 @@
 CREATE TRIGGER decrease_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
