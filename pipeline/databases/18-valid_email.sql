064ae2b04bf0c7ded88af9b84a6285f8eec07055
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-11-01 23:40:11 +0200

    updated

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER reset_valid_email_on_email_change
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
-END$$
+	IF STRCMP(old.email, new.email) <> 0 THEN
+	   SET new.valid_email = 0;
+	END IF;
+END $$
