82ff414dc93189e2d1adfda9117787a4b923c51b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-11-01 21:03:33 +0000

    Best genre

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,6 @@
+-- a SQL script that lists all genres in the hbtn_0d_tvshows_rate database by their rating sum, sorted in descending order
+SELECT tv_genres.name AS name, SUM(rating) AS rating
+FROM tv_genres
+JOIN tv_shows ON tv_shows.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
