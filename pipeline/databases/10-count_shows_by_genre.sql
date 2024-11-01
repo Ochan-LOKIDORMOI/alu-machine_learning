55c47f3d5b9130edd672612fe52ee05cc0f0ac38
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2024-11-01 23:10:27 +0200

    updated

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- A script to list all genres and the number of shows linked to each genre,ordered by the number of shows in descending order
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM genres
-INNER JOIN tv_show_genres
-ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+SELECT name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC
--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,7 +1,6 @@
 -- The script to list all shows with their rating sum, sorted in descending order by the rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+SELECT title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-JOIN tv_show_ratings 
-ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating DESC
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
+ORDER BY rating DESC;
