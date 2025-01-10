13d49c7cc21ed6e23c565b932c59074b26608f5c
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2025-01-10 14:56:59 +0200

    Load the Model

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-This module defines a class for performing Neural Style Transfer (NST).
+Defines class NST that performs tasks for neural style transfer
 """
+
 
 import numpy as np
 import tensorflow as tf
@@ -9,59 +10,66 @@ import tensorflow as tf
 
 class NST:
     """
-    A class to implement Neural Style Transfer (NST).
+    Performs tasks for Neural Style Transfer
 
-    Class-level attributes:
-        style_layers (list of str): Names of layers used for style extraction.
-        content_layer (str): Name of the layer used for content extraction.
+    public class attributes:
+        style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                        'block4_conv1', 'block5_conv1']
+        content_layer = 'block5_conv2'
 
-    Instance attributes:
-        style_image (numpy.ndarray): The processed image for style reference.
-        content_image (numpy.ndarray): The processed image for content reference.
-        alpha (float): The weight for the content loss term.
-        beta (float): The weight for the style loss term.
+    instance attributes:
+        style_image: preprocessed style image
+        content_image: preprocessed style image
+        alpha: weight for content cost
+        beta: weight for style cost
 
-    Methods:
-        __init__(style_image, content_image, alpha, beta):
-            Initializes the NST object with style and content images, and optional weights.
-        scale_image(image):
-            Scales an input image for style transfer by resizing and normalizing.
+    class constructor:
+        def __init__(self, style_image, content_image, alpha=1e4, beta=1)
+
+    static methods:
+        def scale_image(image):
+            rescales an image so the pixel values are between 0 and 1
+                and the largest side is 512 pixels
     """
-    
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """
-        Initializes the NST object for style transfer.
+        Class constructor for Neural Style Transfer class
 
-        Args:
-            style_image (numpy.ndarray): The image representing the desired style.
-            content_image (numpy.ndarray): The image to be used as content reference.
-            alpha (float): The content loss weight factor (default is 1e4).
-            beta (float): The style loss weight factor (default is 1).
+        parameters:
+            style_image [numpy.ndarray with shape (h, w, 3)]:
+                image used as style reference
+            content_image [numpy.ndarray with shape (h, w, 3)]:
+                image used as content reference
+            alpha [float]: weight for content cost
+            beta [float]: weight for style cost
 
-        Raises:
-            TypeError: If any input is of the wrong type or dimension.
+        Raises TypeError if input are in incorrect format
+        Sets TensorFlow to execute eagerly
+        Sets instance attributes
         """
-        if not isinstance(style_image, np.ndarray) or len(style_image.shape) != 3:
-            raise TypeError("style_image should be a 3D numpy array with shape (h, w, 3)")
-        if not isinstance(content_image, np.ndarray) or len(content_image.shape) != 3:
-            raise TypeError("content_image should be a 3D numpy array with shape (h, w, 3)")
-
-        # Check valid dimensions
+        if type(style_image) is not np.ndarray or \
+           len(style_image.shape) != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if type(content_image) is not np.ndarray or \
+           len(content_image.shape) != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
         style_h, style_w, style_c = style_image.shape
         content_h, content_w, content_c = content_image.shape
         if style_h <= 0 or style_w <= 0 or style_c != 3:
-            raise TypeError("style_image must have valid height, width, and 3 color channels")
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
         if content_h <= 0 or content_w <= 0 or content_c != 3:
-            raise TypeError("content_image must have valid height, width, and 3 color channels")
-        
-        # Validate weights
-        if not isinstance(alpha, (float, int)) or alpha < 0:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if (type(alpha) is not float and type(alpha) is not int) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
-        if not isinstance(beta, (float, int)) or beta < 0:
+        if (type(beta) is not float and type(beta) is not int) or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
         tf.enable_eager_execution()
@@ -71,76 +79,77 @@ class NST:
         self.alpha = alpha
         self.beta = beta
 
-        self._initialize_model()
+        self.load_model()
 
     @staticmethod
     def scale_image(image):
         """
-        Resizes and normalizes an image to prepare it for the neural network.
+        Rescales an image such that its pixels values are between 0 and 1
+            and its largest side is 512 pixels
 
-        The resized image will have its largest side scaled to 512 pixels,
-        and the pixel values will be normalized to the range [0, 1].
+        parameters:
+            image [numpy.ndarray of shape (h, w, 3)]:
+                 image to be rescaled
 
-        Args:
-            image (numpy.ndarray): The input image to be processed.
+        Scaled image should be tf.tensor with shape (1, h_new, w_new, 3)
+            where max(h_new, w_new) is 512 and
+            min(h_new, w_new) is scaled proportionately
+        Image should be resized using bicubic interpolation.
+        Image's pixels should be rescaled from range [0, 255] to [0, 1].
 
-        Returns:
-            tf.Tensor: The scaled and normalized image.
+        returns:
+            the scaled image
         """
-        if not isinstance(image, np.ndarray) or len(image.shape) != 3:
-            raise TypeError("image must be a 3D numpy array with shape (h, w, 3)")
-
+        if type(image) is not np.ndarray or len(image.shape) != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
         h, w, c = image.shape
         if h <= 0 or w <= 0 or c != 3:
-            raise TypeError("image must have valid height, width, and 3 color channels")
-
-        # Calculate new dimensions
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
         if h > w:
-            new_h = 512
-            new_w = int(w * (512 / h))
+            h_new = 512
+            w_new = int(w * (512 / h))
         else:
-            new_w = 512
-            new_h = int(h * (512 / w))
+            w_new = 512
+            h_new = int(h * (512 / w))
 
-        # Resize image using bicubic interpolation
-        resized_image = tf.image.resize_bicubic(np.expand_dims(image, axis=0), size=(new_h, new_w))
-        # Normalize pixel values to [0, 1]
-        normalized_image = resized_image / 255
-        normalized_image = tf.clip_by_value(normalized_image, 0, 1)
-        
-        return normalized_image
+        resized = tf.image.resize_bicubic(np.expand_dims(image, axis=0),
+                                          size=(h_new, w_new))
+        rescaled = resized / 255
+        rescaled = tf.clip_by_value(rescaled, 0, 1)
+        return (rescaled)
 
-    def _initialize_model(self):
-        """
-        Loads the pre-trained VGG19 model and configures it to extract features
-        from the layers used for style and content.
-
-        The VGG19 model is used as a feature extractor, and the outputs from the
-        layers defined in `style_layers` and `content_layer` are stored in the
-        instance attribute `model`.
-        """
-        # Load the VGG19 model with ImageNet weights
-        vgg_model = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
-        vgg_model.save("VGG19_base_model")
-
+    def load_model(self):
+        '''
+            creates the model used to calculate cost
+            the model should use the VGG19 Keras model as a base
+            the model’s input should be the same as the VGG19 input
+            the model’s output should be a list containing the outputs
+            of the VGG19 layers listed in style_layers followed by content
+            _layer
+            saves the model in the instance attribute model
+        '''
+        VGG19_model = tf.keras.applications.VGG19(include_top=False,
+                                                  weights='imagenet')
+        VGG19_model.save("VGG19_base_model")
         custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
 
-        # Reload the model with custom layers
-        vgg = tf.keras.models.load_model("VGG19_base_model", custom_objects=custom_objects)
+        vgg = tf.keras.models.load_model("VGG19_base_model",
+                                         custom_objects=custom_objects)
 
         style_outputs = []
         content_output = None
 
-        # Extract outputs from the specified layers
         for layer in vgg.layers:
             if layer.name in self.style_layers:
                 style_outputs.append(layer.output)
-            if layer.name == self.content_layer:
+            if layer.name in self.content_layer:
                 content_output = layer.output
+
             layer.trainable = False
 
-        # Combine style and content layer outputs
-        model_outputs = style_outputs + [content_output]
+        outputs = style_outputs + [content_output]
 
-        # Create a new model with the desired outputs
-        self.model = tf.keras.models.Model(vgg.input, model_outputs)
+        model = tf.keras.models.Model(vgg.input, outputs)
+        self.model = model
