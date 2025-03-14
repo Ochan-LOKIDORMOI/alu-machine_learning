5d39a035bdd42b0ad6a558194ef2e3e6a66ff6a2
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2025-03-13 17:15:19 -0700

    3

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-
 """
-a function that tests for the optimum
-number of clusters by variance
+Defines function that tests for the optimum number of clusters by variance
 """
 
 
@@ -12,9 +10,34 @@ variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    '''
-    A function that tests for the optimum
-    '''
+    """
+    Tests for the optimum number of clusters by variance
+
+    parameters:
+        X [numpy.ndarray of shape (n, d)]:
+            contains the dataset used for K-means clustering
+            n: the number of data points
+            d: the number of dimensions for each data point
+        kmin [positive int]:
+            containing the minimum number of clusters to check for (inclusive)
+        kmax [positive int]:
+            containing the maximum number of clusters to check for (inclusive)
+        iterations [positive int]:
+            containing the maximum number of iterations for K-means
+
+    function should analyze at least 2 different cluster sizes
+
+    should use at most 2 loops
+
+    returns:
+        results, d_vars:
+            results [list]:
+                containing the output of K-means for each cluster size
+            d_vars [list]:
+                containing the difference in variance from the smallest cluster
+                    size for each cluster size
+        or None, None on failure
+    """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
     if not isinstance(kmin, int) or kmin <= 0:
@@ -39,3 +62,4 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
             var = new_var
         d_vars.append(var - new_var)
     return results, d_vars
+
